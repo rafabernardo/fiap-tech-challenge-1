180757ca2332a21d1f478bfe5305bb7777805bc1
Alceu Ramos <alceu.rcjr@gmail.com>
2024-07-24 22:22:13 -0300

    feat(app): Clean up app file and use port from environment variables

--- src/adapters/driver/entrypoints/v1/v1.py
+++ src/adapters/driver/entrypoints/v1/v1.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from src.adapters.driver.entrypoints.v1.register import router as register_router
 
 router = APIRouter()
 
@@ -7,8 +6,3 @@ router = APIRouter()
 @router.get("/ping")
 async def read_main():
     return {"msg": "pong"}
-
-
-def create_routes():
-    router.include_router(register_router, prefix="/register")
-    # app.include_router(settings_router)
