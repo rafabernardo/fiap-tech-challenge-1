889f596c305c56c0997c8f469a79295b23d8879b
Rafaela Rodrigues <rafaela@highnote.io>
2024-07-25 21:47:08 -0300

    feat(models): Add class of OrderItem to Order

--- src/core/domain/models/order.py
+++ src/core/domain/models/order.py
@@ -5,10 +5,17 @@ from typing import Optional
 from core.domain.models import User
 from core.domain.models import Product
 
+class OrderItem(BaseModel):
+    order_item_id: int
+    product: Product
+    quantity: int
+    price: int
+
+
 class Order(BaseModel):
     order_id: int
     status: str
-    products: list[Product]
+    products: list[OrderItem]
     created_at: datetime
     updated_at: datetime
     owner: Optional[User] = None
