3249855ef499ec043a1845887837ae1941fe1b16
Alceu Ramos <alceu.rcjr@gmail.com>
2024-07-27 12:31:42 -0300

    feat(register): Remove register files

--- src/adapters/driver/entrypoints/v1/model/register.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Register(BaseModel):
-    id: int
-    name: str | None = None
-    email: str | None = None
-    cpf: str | None = None
--- src/core/domain/models/register.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Register(BaseModel):
-    id: int
-    name: str | None = None
-    email: str | None = None
-    cpf: str | None = None
