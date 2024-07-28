a39f8fdbd9760d1ae057863a3489aac7a4ea7223
Alceu Ramos <alceu.rcjr@gmail.com>
2024-07-27 23:33:35 -0300

    fix(order): Adjust types and id attributes

--- src/core/domain/models/order.py
+++ src/core/domain/models/order.py
@@ -1,19 +1,20 @@
-from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
-from core.domain.models import User
-from core.domain.models import Product
+from core.domain.models.product import Product
+from core.domain.models.user import User
+from pydantic import BaseModel
+
 
 class OrderItem(BaseModel):
-    order_item_id: int
+    id: int
     product: Product
-    quantity: int
-    price: int
+    quantity: float
+    price: float
 
 
 class Order(BaseModel):
-    order_id: int
+    id: int
     status: str
     products: list[OrderItem]
     created_at: datetime
