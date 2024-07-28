e0d80c0d44b3767a57fb8e9235e895af80384a1b
Alceu Ramos <alceu.rcjr@gmail.com>
2024-07-27 23:35:17 -0300

    refactor(order): Use enum to define status fields

--- src/core/domain/models/order.py
+++ src/core/domain/models/order.py
@@ -1,9 +1,26 @@
 from datetime import datetime
+from enum import Enum
 from typing import Optional
 
 from core.domain.models.product import Product
 from core.domain.models.user import User
 from pydantic import BaseModel
+
+
+class Status(Enum, str):
+    pending = "pending"
+    confirmed = "confirmed"
+    received = "received"
+    being_prepared = "being_prepared"
+    finished = "finished"
+    returned = "returned"
+    canceled = "canceled"
+
+
+class PaymentStatus(Enum, str):
+    paid = "paid"
+    pending = "pending"
+    canceled = "canceled"
 
 
 class OrderItem(BaseModel):
@@ -15,9 +32,9 @@ class OrderItem(BaseModel):
 
 class Order(BaseModel):
     id: int
-    status: str
+    status: Status
     products: list[OrderItem]
     created_at: datetime
     updated_at: datetime
     owner: Optional[User] = None
-    payment_status: str
+    payment_status: PaymentStatus
