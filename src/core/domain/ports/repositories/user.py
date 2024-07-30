fd8e0c4ec52f4c08cd618d208279bbd98b6f0bfe
Alceu Ramos <alceu.rcjr@gmail.com>
2024-07-29 23:10:41 -0300

    feat(User): Simplify user port repository

--- src/core/domain/ports/repositories/user.py
+++ src/core/domain/ports/repositories/user.py
@@ -6,22 +6,17 @@ from core.domain.models.user import User
 class UserRepositoryInterface(abc.ABC):
 
     def __init__(self):
-        self.seen: set[User] = set()
+        ...
 
     def add(self, user: User) -> None:
         self._add(user)
-        self.seen.add(user)
 
     def get_by_id(self, id: int) -> User:
         user = self._get_by_id(id)
-        if user:
-            self.seen.add(user)
         return user
 
     def list_users(self) -> list[User]:
         users = self._list_users()
-        if users:
-            self.seen.union(set(users))
         return users
 
     @abc.abstractmethod
