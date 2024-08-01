97b45f6739796deb720e34bfe5dc8c086db77ae2
Rafaela <rafabernardo.r@gmai.com>
2024-07-31 22:55:54 -0300

    feat(utils): Add prepare document to db

--- src/adapters/driven/repositories/utils.py
+++ src/adapters/driven/repositories/utils.py
@@ -1,3 +1,20 @@
-def replace_id_key(data: dict):
+import copy
+from datetime import datetime
+
+
+def replace_id_key(document: dict):
+    data = copy.deepcopy(document)
     if "_id" in data:
         data["id"] = str(data.pop("_id"))
+    return data
+
+
+def prepare_document_to_db(document: dict):
+    data = copy.deepcopy(document)
+    now = datetime.now()
+
+    data.pop("id", None)
+    data["updated_at"] = now
+    if data.get("created_at") is None:
+        data["created_at"] = now
+    return data
