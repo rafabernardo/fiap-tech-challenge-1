0bac6dc8608899af9ab914c8178c21984d89cacf
Alceu Ramos <alceu.rcjr@gmail.com>
2024-08-01 22:13:57 -0300

    feat: Add register Order models

---
+++ src/adapters/driver/entrypoints/v1/models/order.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class OrderItem(BaseModel):
+    product_id: str
+    quantity: int
+    price: int
+
+
+class RegisterOrderV1Request(BaseModel):
+    owner_id: str | None = None
+    products: list[OrderItem]
+
+
+class RegisterOrderV1Response(BaseModel):
+    id: str | None = None
+    owner_id: str | None = None
+
+    status: str
+    products: list[OrderItem]
+    payment_status: str
+
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
