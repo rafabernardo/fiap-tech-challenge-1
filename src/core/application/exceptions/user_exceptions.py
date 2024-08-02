b488400fe39e82efa8e9793963970b12d1e08405
Rafaela Rodrigues <rafaela@highnote.io>
2024-08-01 21:13:43 -0300

    feat: Add UserInvalidFormatDataError exception

--- src/core/application/exceptions/user_exceptions.py
+++ src/core/application/exceptions/user_exceptions.py
@@ -2,3 +2,9 @@ class UserAlreadyExistsError(Exception):
     def __init__(self, message: str):
         self.message = message
         super().__init__(self.message)
+
+
+class UserInvalidFormatDataError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
