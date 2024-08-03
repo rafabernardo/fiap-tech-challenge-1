90bfc2e1e4e2f17193f579f4ee28c87c21a5a41e
Rafaela <rafabernardo.r@gmai.com>
2024-08-03 15:44:02 -0300

    feat: Add order list paginated endpoint

--- src/adapters/driver/entrypoints/v1/orders.py
+++ src/adapters/driver/entrypoints/v1/orders.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, HTTPException, Query, Response, status
 
 from adapters.driven.repositories.order_repository import OrderMongoRepository
+from adapters.driven.repositories.utils import get_pagination_info
 from adapters.driver.entrypoints.v1.models.order import (
+    ListOrderV1Response,
+    OrderV1Response,
     RegisterOrderV1Request,
     RegisterOrderV1Response,
 )
@@ -14,9 +17,27 @@ HEADER_CONTENT_TYPE_APPLICATION_JSON = "application/json"
 router = APIRouter(prefix="/order")
 
 
-@router.get("")
-async def list_users():
-    return {"msg": "pong"}
+@router.get("", response_model=ListOrderV1Response)
+async def list_users(
+    page: int = Query(default=1, gt=0),
+    page_size: int = Query(default=10, gt=0, le=100),
+) -> ListOrderV1Response:
+    order_repository = OrderMongoRepository()
+    order_service = OrderService(repository=order_repository)
+
+    orders = order_service.list_orders(
+        filter={}, page=page, page_size=page_size
+    )
+    total_orders = order_service.count_orders(filter={})
+    pagination_info = get_pagination_info(
+        total_results=total_orders, page=page, page_size=page_size
+    )
+    listed_orders = [OrderV1Response(**order.model_dump()) for order in orders]
+
+    paginated_orders = ListOrderV1Response(
+        **pagination_info.model_dump(), results=listed_orders
+    )
+    return paginated_orders
 
 
 @router.get("/{id}")
