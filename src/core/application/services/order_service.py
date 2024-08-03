677d14d9a0c26eed434ca97472098b67fc95e2be
Rafaela <rafabernardo.r@gmai.com>
2024-08-02 23:09:14 -0300

    feat: Add order number to order object when register

--- src/core/application/services/order_service.py
+++ src/core/application/services/order_service.py
@@ -1,5 +1,8 @@
+from core.application.services.order_number_service import OrderNumberService
 from core.domain.models.order import Order
 from core.domain.ports.repositories.order import OrderRepositoryInterface
+
+order_number_service = OrderNumberService()
 
 
 class OrderService:
@@ -7,5 +10,6 @@ class OrderService:
         self.repository = repository
 
     def register_order(self, order: Order) -> Order:
+        order.order_number = order_number_service.get_next_order_number()
         new_order = self.repository.add(order)
         return new_order
