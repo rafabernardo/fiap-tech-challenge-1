44e05e3d743c7c0340ba1643647c0082a1407730
Rafaela <rafabernardo.r@gmai.com>
2024-08-03 15:41:26 -0300

    feat: Add get_pagination_info utils function

--- src/adapters/driven/repositories/utils.py
+++ src/adapters/driven/repositories/utils.py
@@ -1,6 +1,8 @@
 import copy
 from datetime import datetime
 
+from adapters.driver.entrypoints.v1.models.page import PageV1Response
+
 
 def replace_id_key(document: dict):
     data = copy.deepcopy(document)
@@ -18,3 +20,21 @@ def prepare_document_to_db(document: dict):
     if data.get("created_at") is None:
         data["created_at"] = now
     return data
+
+
+def get_pagination_info(
+    total_results=int, page=int, page_size=int
+) -> PageV1Response:
+    total_pages = (total_results + page_size - 1) // page_size
+    has_next = page < total_pages
+    has_previous = page > 1
+
+    pagination_info = PageV1Response(
+        total_results=total_results,
+        page=page,
+        page_size=page_size,
+        total_pages=total_pages,
+        has_next=has_next,
+        has_previous=has_previous,
+    )
+    return pagination_info
