e2ab2ee862b763ac4b66002fab32f0be94146167
Alceu Ramos <alceu.rcjr@gmail.com>
2024-08-04 01:59:39 -0300

    feat: Add internal server error customized exception

--- src/adapters/driver/entrypoints/v1/exceptions/commons.py
+++ src/adapters/driver/entrypoints/v1/exceptions/commons.py
@@ -1,6 +1,13 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 
 
 class NoDocumentsFoundException(HTTPException):
     def __init__(self, detail: str = "No document found"):
-        super().__init__(status_code=404, detail=detail)
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+class InternalServerErrorException(HTTPException):
+    def __init__(self, detail: str = "Internal server error"):
+        super().__init__(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail
+        )
