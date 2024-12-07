7866b10076333bf6655f08408e4614edf7f6738b
Alceu Ramos <alceu.rcjr@gmail.com>
2024-12-07 11:16:31 -0300

    feat: Edit alembic version 0 to use id as integer

--- alembic/versions/0_44fef50d7d68_create_users_table.py
+++ alembic/versions/0_44fef50d7d68_create_users_table.py
@@ -1,11 +1,11 @@
 """Initial migration
 
 Revision ID: 44fef50d7d68
-Revises: 
+Revises:
 Create Date: 2024-11-30 20:53:54.519296
 
 """
-import uuid
+
 from typing import Sequence, Union
 
 import sqlalchemy as sa
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '44fef50d7d68'
+revision: str = "44fef50d7d68"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -23,7 +23,9 @@ def upgrade() -> None:
     # Create the 'users' table
     op.create_table(
         "users",
-        sa.Column("id", sa.UUID(), nullable=False, primary_key=True),
+        sa.Column(
+            "id", sa.Integer, autoincrement=True, nullable=False, primary_key=True
+        ),
         sa.Column("name", sa.String, nullable=False),
         sa.Column("email", sa.String, nullable=False),
         sa.Column("cpf", sa.String, nullable=True),
@@ -35,16 +37,10 @@ def upgrade() -> None:
     op.create_index("ix_users_id", "users", ["id"], unique=False)
     op.create_index("ix_users_name", "users", ["name"], unique=False)
     op.create_index("ix_users_cpf", "users", ["cpf"], unique=False)
-    op.create_index("ix_users_created_at", "users", ["created_at"], unique=False)
-    op.create_index("ix_users_updated_at", "users", ["updated_at"], unique=False)
-
-
 
 
 def downgrade() -> None:
     # Drop indexes
-    op.drop_index("ix_users_updated_at", table_name="users")
-    op.drop_index("ix_users_created_at", table_name="users")
     op.drop_index("ix_users_cpf", table_name="users")
     op.drop_index("ix_users_name", table_name="users")
     op.drop_index("ix_users_id", table_name="users")
