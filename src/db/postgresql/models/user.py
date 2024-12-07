b2a05895610216acb1844ac98a85e50b3c38b7b6
alceuramos <56130695+alceuramos@users.noreply.github.com>
2024-12-07 10:42:58 -0300

    Remove commented code
    Co-authored-by: Rafaela Rodrigues <rafabernardo.r@gmail.com>

--- src/db/postgresql/models/user.py
+++ src/db/postgresql/models/user.py
@@ -9,7 +9,7 @@ class UserModel(Base):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
 
-    # id = Column(String, primary_key=True, index=True)
+    
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False, index=True)
     email = Column(String, nullable=False)
